74b5c4a4eff2fd7ff7f1e9aa04d205a6f5d09705
Carlos Jaime <jaime_andrek@hotmail.com>
2021-10-03 15:40:31 -0300

    Metodos magicos __GET __SET

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
 require_once(dirname(__FILE__,2) . '/src/models/User.php');
 $user = new User(['name'=>'carlos', 'email'=>'jaime_andrek@hotmail.com']);
-print_r($user);
+echo $user->email;
 
 
