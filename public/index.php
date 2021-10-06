548fe5c4c48534a9a32b06fb8fde0b2f48824235
Carlos Jaime <jaime_andrek@hotmail.com>
2021-10-05 21:58:20 -0300

    Obtendo Usuario do banco

--- public/index.php
+++ public/index.php
@@ -3,10 +3,14 @@
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
 require_once(dirname(__FILE__,2) . '/src/models/User.php');
 
-//$user = new User(['name'=>'carlos', 'email'=>'jaime_andrek@hotmail.com']);
-echo User::getSelect(['name'=>'Chaves', 'email'=>'chaves@cod3r.com.br'],'id, name');
+$user = new User(['name'=>'carlos', 'email'=>'jaime_andrek@hotmail.com']);
+echo var_dump(User::get(['name'=>'Chaves', 'email'=>'chaves@cod3r.com.br'],'id, name'));
 echo '<br>';
-echo User::getSelect(['id'=>'1'],'id, name');
+foreach(User::get([],'id, name, email') as $value){
+    echo $value->name;
+    echo '<br>';
+    echo $value->email;
+}
 
 
 
