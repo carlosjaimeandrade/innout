99cee220363fd6df6e68ccdb18688f64888d3bbb
Carlos Jaime <jaime_andrek@hotmail.com>
2021-10-06 21:17:17 -0300

    Login front

--- public/index.php
+++ public/index.php
@@ -1,16 +1,9 @@
 <?php
 
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
-require_once(dirname(__FILE__,2) . '/src/models/User.php');
+require_once(dirname(__FILE__,2) . '/src/views/login.php');
 
-$user = new User(['name'=>'carlos', 'email'=>'jaime_andrek@hotmail.com']);
-echo var_dump(User::get(['name'=>'Chaves', 'email'=>'chaves@cod3r.com.br'],'id, name'));
-echo '<br>';
-foreach(User::get([],'id, name, email') as $value){
-    echo $value->name;
-    echo '<br>';
-    echo $value->email;
-}
+
 
 
 
