22b81b015b1e1550b541e3af36aa0c5e0ca25b11
Carlos Jaime <jaime_andrek@hotmail.com>
2021-10-08 22:32:39 -0300

    Classe User #1

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
-require_once(dirname(__FILE__,2) . '/src/views/login.php');
+require_once(VIEW_PATH . '/login.php');
 
 
 
---
+++ src/models/Login.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once(realpath(MODEL_PATH . '/User.php'));
+
+class Login extends Model
+{
+    public function checklogin(){
+         $user = User::getOne(['email' => $this->email]);
+         
+    }
+}
