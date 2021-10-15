ca1ddfba32176cbb14584053c3ef5dbbf1f18ca7
Carlos jaime <jaime_andrek@hotmail.com>
2021-10-15 19:48:49 -0300

    Implementação classe Login

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
 require_once(dirname(__FILE__,2) . '/src/config/config.php');
-require_once(VIEW_PATH . '/login.php');
+//require_once(VIEW_PATH . '/login.php');
 
+require_once(MODEL_PATH . '/login.php');
 
+$login = new Login([
+    'email' => 'admin@cod3r.com.br',
+    'password' => 'a'
+]);
 
-
-
+try{
+    $login->checklogin();
+    echo "Deu certo ";
+} catch(Exception $e){
+    echo 'problema de login';
+}
--- src/models/Login.php
+++ src/models/Login.php
@@ -6,6 +6,11 @@ class Login extends Model
 {
     public function checklogin(){
          $user = User::getOne(['email' => $this->email]);
-         
+         if($user){
+             if(password_verify($this->password, $user->password)){
+                 return $user;
+             }
+         }
+         throw new Exception();
     }
 }
