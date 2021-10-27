7bffd769554e3b3546f6902f8ac0f65d2b708a37
Carlos jaime <jaime_andrek@hotmail.com>
2021-10-27 20:21:13 -0300

    Direcionamento htaccess

--- src/config/config.php
+++ src/config/config.php
@@ -4,12 +4,11 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8','portuguese');
 
 
 //pastas
-define('MODEL_PATH', realpath(dirname(__FILE__). '/../models'));
-define('VIEW_PATH', realpath(dirname(__FILE__). '/../views'));
-define('TEMPLATE_PATH', realpath(dirname(__FILE__). '/../views/template'));
-define('CONTROLLER_PATH', realpath(dirname(__FILE__). '/../controllers'));
-define('EXCEPTION_PATH', realpath(dirname(__FILE__). '/../exceptions'));
-
+define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
+define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
+define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 //arquivos
 require_once(realpath(dirname(__FILE__). '/database.php'));
 require_once(realpath(dirname(__FILE__). '/loader.php'));
