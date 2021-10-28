d2be4df4acc3af5dda0119e4059392a8ffb0447f
Carlos jaime <jaime_andrek@hotmail.com>
2021-10-28 19:43:05 -0300

    Menu lateral

---
+++ src/views/template/left.php
@@ -0,0 +1,34 @@
+<aside class='sidebar'>
+    <nav class="menu mt-3">
+        <ul class="nav-list"> 
+            <li class='nav-item'>
+                <a href="day_records.php">
+                    <i class='icofont-ui-check mr-2'>
+                        Registrar ponto
+                    </i>
+                </a>
+            </li>
+            <li class='nav-item'>
+                <a href=".php">
+                    <i class='icofont-ui-calendar mr-2'>
+                        Relatorio mensal
+                    </i>
+                </a>
+            </li>
+            <li class='nav-item'>
+                <a href=".php">
+                    <i class='icofont-chart-histogram mr-2'>
+                        Relatório gerencial
+                    </i>
+                </a>
+            </li>
+            <li class='nav-item'>
+                <a href=".php">
+                    <i class='icofont-users mr-2'>
+                        Usuarios
+                    </i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
