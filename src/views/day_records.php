cdeb5f49e5406f61c98e360882d8e4d5f24ecaad
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-04 19:52:25 -0300

    Tela bater ponto

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,5 +1,31 @@
 <main class='content'>
-<h1>ok</h1>
-<h1>ok</h1>
-<h1>ok</h1>
+    <div class="content-title mb-4">
+        <i class="icon icofont-check-alt mr-2"></i>
+        <div>
+            <h1>Registrar</h1>
+            <h2>Mantenha seu ponto consistente!</h2>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>10 de Janeiro de 2025</h3>
+            <p class="mb-0">Os batimentos efetuados hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 1: ---</span>
+                <span>Saida 2: ---</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 1: ---</span>
+                <span>Saida 2: ---</span>
+            </div>
+        </div>
+    </div>
+    <div class='card-footer d-flex justify-content-center'>
+        <a href="???" class="btn btn-success btn-log">
+            <i class="icofont-check mr-1"></i>
+            Batendo o ponto
+        </a>
+    </div>
 </main>
