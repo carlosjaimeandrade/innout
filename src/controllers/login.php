e7a7c7cac56ffdc5a05932685241f55f81978aca
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-04 20:16:15 -0300

    Logout, session

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -2,12 +2,14 @@
 
 
 loadModel('Login');
+session_start();
 $exception = null;
 
 if(count($_POST) > 0){
     $login = new Login($_POST);
     try{
         $users = $login->checklogin();
+        $_SESSION['user'] = $users;
         header("location: day_records.php");
     }catch(AppException $e){
         $exception  = $e;
