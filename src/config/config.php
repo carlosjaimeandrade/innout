14aa9170bc2d920cfbfa410a540a86c8d5674ffc
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-08 20:35:33 -0300

    Data Generator

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,8 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8','portuguese');
 
+//constante 
+define('DAILY_TIME', 60 * 60 * 8);
 
 //pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
