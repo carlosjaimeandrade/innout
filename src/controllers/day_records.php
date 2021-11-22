3c760606b14400ed2d8c68ba90db0bb30ba9f7f0
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-22 20:04:43 -0300

    Ajuste home

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -10,7 +10,7 @@ $today = strftime("%d de %B de %Y", $date);
 $user = $_SESSION['user'];
 
 $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
-var_dump($records);
+
 
 loadTemplateView('day_records',[
     'today'=>$today,
