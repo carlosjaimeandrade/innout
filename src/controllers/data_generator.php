c5f36201d237e624409a14bb8b01aca82afbbb08
Carlos jaime <jaime_andrek@hotmail.com>
2021-11-22 19:27:19 -0300

    data_generator ajustes

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -56,6 +56,8 @@ function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $
         $columns['work_date'] = $currentDate;
     }
 }
-
+$lastMonth = strtotime('first day of last month');
 populateWorkingHours(1, date('Y-m-1'), 70,20,10);
+populateWorkingHours(3, date('Y-m-1', $lastMonth), 20,75,5);
+populateWorkingHours(4, date('Y-m-1',$lastMonth), 20,10,70);
 echo "tudo certo";
